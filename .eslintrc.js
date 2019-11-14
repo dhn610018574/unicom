feb32404d024ea0c0b3a202444cf532ecc78b3bb
unknown <shs-cyhlwzydhn@chinaunicom.cn>
2019-11-14 17:24:34 +0800

    处理兼容性

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,29 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "generator-star-spacing": "off",
+    "no-mixed-operators": 0,
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        "singleline": 5,
+        "multiline": {
+          "max": 1,
+          "allowFirstLine": false
+        }
+      }
+    ],
+    "vue/attribute-hyphenation": 0,
+    "vue/html-self-closing": 0,
+    "vue/component-name-in-template-casing": 0,
+    "vue/html-closing-bracket-spacing": 0,
+    "vue/singleline-html-element-content-newline": 0,
+    "vue/no-unused-components": 0,
+    "vue/multiline-html-element-content-newline": 0,
+    "vue/no-use-v-if-with-v-for": 0,
+    "vue/html-closing-bracket-newline": 0,
+    "vue/no-parsing-error": 0
   },
   parserOptions: {
     parser: 'babel-eslint'
